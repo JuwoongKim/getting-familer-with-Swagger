0a33db0b5e93619dc0e98d91fe6122c9fef1922e
juwoong kim <wwong7247@gmail.com>
2024-01-26 16:34:46 +0900

    학습용 프로젝트 초기화

---
+++ src/main/java/com/juwwong/gettingfamilerwithswagger/GettingFamilerWithSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.juwwong.gettingfamilerwithswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingFamilerWithSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingFamilerWithSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juwwong/gettingfamilerwithswagger/GettingFamilerWithSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juwwong.gettingfamilerwithswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingFamilerWithSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
